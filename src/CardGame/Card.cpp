e27f6e0e1f7a047da06015849d7e815638a940ba
Maxim Melkonian <mmelk057@uottawa.ca>
2020-11-29 23:08:43 -0500

    CSI2372: Finish Card implementations

--- src/CardGame/Card.cpp
+++ src/CardGame/Card.cpp
@@ -27,7 +27,7 @@ int fetchCoinsPerCard(const std::map<int, int>& value_mappings, int coins) {
 		}
 	}
 
-	return positions[coins];
+	return positions[coins] == INT_MAX ? -1 : positions[coins];
 }
 
 /* Non-member function impl of left-shift operator to stream Card class */
@@ -55,4 +55,129 @@ void Blue::print(ostream& out) const {
 	out.write(typeid(this).name(), 1);
 }
 
+//#######  Chili Card Start ###############################################
+int Chili::getCardsPerCoin(int coins) {
+	std::map<int, int> coinsToCards;
+	coinsToCards[1] = 3;
+	coinsToCards[2] = 6;
+	coinsToCards[3] = 8;
+	coinsToCards[4] = 9;
+	return fetchCoinsPerCard(coinsToCards, coins);
+}
 
+string Chili::getName() {
+	return typeid(this).name();
+}
+
+void Chili::print(ostream& out) const {
+	out.write(typeid(this).name(), 1);
+}
+
+//######## Stink Card Start ###############################################
+int Stink::getCardsPerCoin(int coins) {
+	std::map<int, int> coinsToCards;
+	coinsToCards[1] = 3;
+	coinsToCards[2] = 5;
+	coinsToCards[3] = 7;
+	coinsToCards[4] = 8;
+	return fetchCoinsPerCard(coinsToCards, coins);
+}
+
+string Stink::getName() {
+	return typeid(this).name();
+}
+
+void Stink::print(ostream& out) const {
+	out.write(typeid(this).name(), 1);
+}
+
+//######### Green Card Start ##############################################
+int Green::getCardsPerCoin(int coins) {
+	std::map<int, int> coinsToCards;
+	coinsToCards[1] = 3;
+	coinsToCards[2] = 5;
+	coinsToCards[3] = 6;
+	coinsToCards[4] = 7;
+	return fetchCoinsPerCard(coinsToCards, coins);
+}
+
+string Green::getName() {
+	return typeid(this).name();
+}
+
+void Green::print(ostream& out) const {
+	out.write(typeid(this).name(), 1);
+}
+
+//######### Soy Card Start ################################################
+int Soy::getCardsPerCoin(int coins) {
+	std::map<int, int> coinsToCards;
+	coinsToCards[1] = 2;
+	coinsToCards[2] = 4;
+	coinsToCards[3] = 6;
+	coinsToCards[4] = 7;
+	return fetchCoinsPerCard(coinsToCards, coins);
+}
+
+string Soy::getName() {
+	return typeid(this).name();
+}
+
+void Soy::print(ostream& out) const {
+	char lowerFirst = tolower((typeid(this).name())[0]);
+	out.write(&lowerFirst, 1);
+}
+
+//########## Black Card Start #############################################
+int Black::getCardsPerCoin(int coins) {
+	std::map<int, int> coinsToCards;
+	coinsToCards[1] = 2;
+	coinsToCards[2] = 4;
+	coinsToCards[3] = 5;
+	coinsToCards[4] = 6;
+	return fetchCoinsPerCard(coinsToCards, coins);
+}
+
+string Black::getName() {
+	return typeid(this).name();
+}
+
+void Black::print(ostream& out) const {
+	char lowerFirst = tolower((typeid(this).name())[0]);
+	out.write(&lowerFirst, 1);
+}
+
+//########## Red Card Start ###############################################
+int Red::getCardsPerCoin(int coins) {
+	std::map<int, int> coinsToCards;
+	coinsToCards[1] = 2;
+	coinsToCards[2] = 3;
+	coinsToCards[3] = 4;
+	coinsToCards[4] = 5;
+	return fetchCoinsPerCard(coinsToCards, coins);
+}
+
+string Red::getName() {
+	return typeid(this).name();
+}
+
+void Red::print(ostream& out) const {
+	out.write(typeid(this).name(), 1);
+}
+
+//########### Garden Card Start #############################################
+int Garden::getCardsPerCoin(int coins) {
+	std::map<int, int> coinsToCards;
+	coinsToCards[2] = 2;
+	coinsToCards[3] = 3;
+	return fetchCoinsPerCard(coinsToCards, coins);
+}
+
+string Garden::getName() {
+	return typeid(this).name();
+}
+
+void Garden::print(ostream& out) const {
+	char lowerFirst = tolower((typeid(this).name())[0]);
+	out.write(&lowerFirst, 1);
+}
